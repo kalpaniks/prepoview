cb355b2d0e94650441cd5d66fb591df97bf39ab1
Aniket <aniketvishwakarma2004@gmail.com>
2025-07-24 23:09:20 +0530

    run format

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,39 @@
-import prisma from "@/lib/prisma";
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { encrypt } from "@/lib/crypto";
- 
+import prisma from '@/lib/prisma';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { encrypt } from '@/lib/crypto';
+
 const handler = NextAuth({
-    adapter : PrismaAdapter(prisma),
-    secret : process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: 'jwt'
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === 'github' && account.access_token) {
+        await prisma.user.update({
+          where: { id: user.id },
+          data: {
+            githubId: account.providerAccountId,
+            accessToken: encrypt(account.access_token),
+          },
+        });
+      }
+      return true;
     },
-    providers : [
-        GithubProvider({
-            clientId : process.env.GITHUB_ID!,
-            clientSecret : process.env.GITHUB_SECRET!
-        })
-    ],
-    callbacks : {
-        async signIn({user, account}){
-            if(account?.provider === "github" && account.access_token){
-                await prisma.user.update({
-                    where : {id : user.id },
-                    data : {
-                        githubId : account.providerAccountId,
-                        accessToken : encrypt(account.access_token)
-                    },
-                });
-            }
-            return true;
-        },
-        async session({session, token}){
-            session.user.id  = token.sub;
-            return session;
-        },
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
     },
+  },
 });
 
-export {handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,16 +1,16 @@
-import NextAuth from 'next-auth'
+import NextAuth from 'next-auth';
 
 declare module 'next-auth' {
   interface Session {
     user: {
-      id?: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
   }
 
   interface User {
-    id: string
+    id: string;
   }
 }
