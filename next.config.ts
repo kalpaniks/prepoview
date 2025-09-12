9ec23eca4facb903839d9b1a879f9f3293d80aa5
Aniket <aniketvishwakarma2004@gmail.com>
2025-09-12 16:20:50 +0530

    feat : add posthog for user analytics

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      { source: '/_ph/static/:path*', destination: 'https://us-assets.i.posthog.com/static/:path*' },
+      { source: '/_ph/:path*', destination: 'https://us.i.posthog.com/:path*' },
+    ];
+  },
   images: {
     remotePatterns: [
       {
